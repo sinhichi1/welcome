db674fbc6a2a87cd1bc62c767a184fc3c96dc573
sinhichi1 <93460637+sinhichi1@users.noreply.github.com>
2021-10-31 13:38:10 +0900

    버전3

---
+++ 4_1_num.py
@@ -0,0 +1,12 @@
+print(-1)
+print(0)
+print(1)
+print(1+1)
+print('2*4', 2*2)
+
+import math
+print('math.sqrt5)', math.sqrt(54))
+print(math.pow(4,4))
+
+import random
+print('random.random()', random.random()py)
